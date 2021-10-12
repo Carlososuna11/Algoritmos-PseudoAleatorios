35a40e706b15ac0da9c2fefc354eb42499b5678b
Carlos Osuna <37004057+Carlososuna11@users.noreply.github.com>
2021-10-12 19:07:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 from algoritmosPseudoAleatorios import *
 
 if __name__ == '__main__':
-    #cuadradosMedios(4,5735,5)
-    #productosMedios(4,5015,5734,5)
-    #multiplicadroConstante(4,6965,9803,5)
-    #lineal(37,19,33,100,4)
-    #linealMaxPeriodoVida(6,3,7,3)
-    #congruencialMultiplicativo(17,2,5)
-    congruencialCuadraticoMaximo(13,3,26,27,27)
+    pass
